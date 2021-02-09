d3fa39b76664784e7b9808383ee55b6ea770ce6a
Andy <anita.wagh3@gmail.com>
2021-02-09 08:46:14 +0530

    adding sign in component and custom bitton

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import { Switch , Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import SingInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route  path='/shop' component={ShopPage}/>
+        <Route  path='/signin' component={SingInAndSignUp}/>
       </Switch>
       
     </div>
